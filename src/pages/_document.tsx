3c50495a927ffcfc83e5a7ff70ee3266bea5592b
sian.me <sian.me@kakaoenterprise.com>
2023-01-29 02:26:34 +0900

    fix: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,11 +13,15 @@ class MyDocument extends Document {
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
           />
           <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link
             rel="apple-touch-icon"
             sizes="192x192"
             href="/apple-touch-icon.png"
           ></link>
+          <link rel="icon" type="image/png" sizes="192x192"  href="/android-chrome-192x192.png" />
+          <link rel="icon" type="image/png" sizes="512x512"  href="/android-chrome-512x512.png" />
           <link
             rel="alternate"
             type="application/rss+xml"
