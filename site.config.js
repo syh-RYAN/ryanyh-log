5d6bcfaa9d8ba0d64be8e2a33b5b9ee29265945a
sian.me <sian.me@kakaoenterprise.com>
2023-01-28 23:37:18 +0900

    fix: build issue

--- site.config.js
+++ site.config.js
@@ -11,10 +11,10 @@ const CONFIG = {
     instagram: '',
   },
   projects: [
-    // {
-    //   name: `morethan-log`,
-    //   href: 'https://github.com/morethanmin/morethan-log'
-    // }
+    {
+      name: '',
+      href: ''
+    }
   ],
   // blog setting (required)
   blog: {
